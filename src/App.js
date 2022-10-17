9731cf3a9e25ee8b24632b181079d08e097efc13
Dave Fisher <dave.j.fish@gmail.com>
2022-10-17 12:21:32 -0700

    basic css for header and Auth

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
-import { useUser } from './context/UserContext';
 import Auth from './components/Auth/Auth';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 
 function App() {
-  const { user } = useUser();
   return (
     <div className="App">
       <Header />
       <Switch>
-        {!user && <Route path="/auth/:type" component={Auth} />}
-        <Route path="/campfire" component={Home} />
-        <Route path='*' component={Auth}/>
+        <Route path="/auth/:type" component={Auth} />
+        <Route path="/campfire" component={Home} /> 
+        <Route path='*' component={Home}>
+          <Redirect to='/campfire' />
+        </Route>
       </Switch>
     </div>
   );
