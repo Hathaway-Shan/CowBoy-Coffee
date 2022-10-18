e4cd77fa39ddfef5cda553d74816e8c09632d36a
Hathaway-Shan <hathaway.shan@gmail.com>
2022-10-18 14:59:19 -0700

    created basic loading animation

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import Auth from './components/Auth/Auth';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Loading from './components/Loading/Loading';
 import Profile from './components/Profile/Profile';
 import ShopDetail from './components/ShopDetail/ShopDetail';
+import useShops from './hooks/useShops';
 
 function App() {
+  const { loading } = useShops();
   return (
     <div className="App">
+      <span>{loading ? <Loading /> : <></>}</span>
       <Header />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
         <Route path="/profile/:id" component={Profile} />
-        <Route path='/campfire/:id' component={ShopDetail} />
+        <Route path="/campfire/:id" component={ShopDetail} />
         <Route path="/campfire" component={Home} />
         <Route path="*">
           <Redirect to="/campfire" />
--- src/components/ShopCards/ShopCards.js
+++ src/components/ShopCards/ShopCards.js
@@ -1,12 +1,12 @@
 import useShops from '../../hooks/useShops';
+import Loading from '../Loading/Loading';
 import './ShopCards.css';
 
 export default function ShopCards() {
-
   const { shops } = useShops();
 
   return (
-    <div className='card-container'>
+    <div className="card-container">
       {shops.map((shop) => (
         <div key={shop.id}>
           <h2>{shop.name}</h2>
