993fa4ba1bbae90d4c3791c4cda88d2af1b1ed79
olivia pasion <oap9820@gmail.com>
2022-10-18 16:12:33 -0700

    added detail component files

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -3,8 +3,10 @@ import useShops from '../../hooks/useShops';
 
 export default function Detail() {
   const { shops } = useShops();
-  
+
   return (
-    <div>Detail</div>
+    <div>
+      
+    </div>
   );
 }
