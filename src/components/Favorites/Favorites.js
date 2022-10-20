20fbb39079683acb3ad58d2e61285c8c03f4bac5
Dave Fisher <dave.j.fish@gmail.com>
2022-10-19 19:56:28 -0700

    getting list of close by favorite shops

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,8 +1,39 @@
+import Loading from '../Loading/Loading';
 
 
 export function Favorites({ favorites, shops, loadFave, error }) {
-  if (loadFave) return <span>we have loaded fave</span>;
+  
+  const getFavorites = () => {
+    const results = favorites.map(fave => (
+      shops.find(shop => {
+        return shop.id === fave.yelp_id;
+      })
+    ));
+    return results;
+  };
+    
+  const results = getFavorites();
+  const userFaves = [];
+  for (let i = 0; i < results.length; i++) {
+    if (results[i] !== undefined) 
+      userFaves.push(results[i]);
+  }
+    
+  if (loadFave) return <span><Loading /></span>;
   if (error) return <h3>{error.message}</h3>;
 
-  return <h1>RIP Pokemon Dungeon</h1>;
+  return (
+    <div>
+      {console.log('userFaves are: ', userFaves)}
+      {userFaves.length === 0 ? <span>add some favorites to see them here</span> : 
+        userFaves.map(
+          fave => (
+            <div key={fave.id}>
+              <h3>{fave.name}</h3>
+            </div>
+          )
+        )
+      }
+    </div>
+  );
 }
