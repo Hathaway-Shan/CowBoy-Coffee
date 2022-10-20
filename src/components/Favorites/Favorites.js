d3afbbae3a7a37984d5c3127642bcdb56db9890e
Hathaway-Shan <hathaway.shan@gmail.com>
2022-10-19 18:28:26 -0700

    did not know that console log was load bearing

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -5,5 +5,6 @@ export function Favorites() {
 
   if (loadFave) return <span>we have loaded fave</span>;
   if (error) return <h3>{error.message}</h3>;
+  console.log('favorites is: ', favorites);
   return <h1>RIP Pokemon Dungeon</h1>;
 }
