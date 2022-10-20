900fde3488381408c1396e97e7150ce0d50192cd
Hathaway-Shan <hathaway.shan@gmail.com>
2022-10-19 18:24:34 -0700

    added class toggle to prevent issue of underline animation pushin menu text to the side

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -1,10 +1,15 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import { useUser } from '../../context/UserContext';
 import { signOut } from '../../services/auth';
 import './NavMenu.css';
 
 export function NavMenu() {
   const { setUser } = useUser();
+  const [navOpen, setNavOpen] = useState(true);
+
+  const handleToggle = () => {
+    setNavOpen((prev) => !prev);
+  };
 
   const handleLogout = async () => {
     try {
@@ -19,21 +24,21 @@ export function NavMenu() {
   return (
     <section className="top-nav">
       <input id="menu-toggle" type="checkbox" />
-      <label className="menu-button-container" htmlFor="menu-toggle">
-        <div className="menu-button"></div>
+      <label className="menu-button-container" htmlFor="menu-toggle" onClick={handleToggle}>
+        <div className="menu-button">{navOpen}</div>
       </label>
       <ul className="menu">
-        <li className="menu-links">
-          <a className="menu-links" href="/campfire">
+        <li className={`${navOpen ? 'menu-links-open' : 'menu-links'}`}>
+          <a className={`${navOpen ? 'menu-links-open' : 'menu-links'}`} href="/campfire">
             Home
           </a>
         </li>
-        <li className="menu-links">
-          <a className="menu-links" href="/the-posse">
+        <li className={`${navOpen ? 'menu-links-open' : 'menu-links'}`}>
+          <a className={`${navOpen ? 'menu-links-open' : 'menu-links'}`} href="/the-posse">
             Meet the Posse
           </a>
         </li>
-        <li className="menu-links" onClick={handleLogout}>
+        <li className={`${navOpen ? 'menu-links-open' : 'menu-links'}`} onClick={handleLogout}>
           Sign Out
         </li>
       </ul>
