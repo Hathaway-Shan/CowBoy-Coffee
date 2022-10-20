048395fd7661f37d0372d6575f06610cacbdd72c
olivia pasion <oap9820@gmail.com>
2022-10-20 14:11:20 -0700

    hats sit properly on dev image

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -14,18 +14,30 @@ export default function AboutUs() {
       <h1>Meet The Cowboy Coffee Posse</h1>
       <div className="dev-container">
         <div className="dev">
+          <div className="hat-container">
+            <img 
+              className="hat -rotate-12" 
+              src={`${process.env.PUBLIC_URL}/assets/hats/olivia-hat.png`}
+            />
+          </div>
           <img className='dev-img' src={`${process.env.PUBLIC_URL}/cowboys/olivia.jpg`} />
           <h3 className="dev-name">Olivia Pasion</h3>
           <a className="site-link" href='https://github.com/Olivia-Pasion'>GitHub</a>
           <a className="site-link" href='https://www.linkedin.com/in/olivia-pasion/'>LinkedIn</a>
         </div>
         <div className="dev">
+          <div className="hat-container">
+            <img className="hat" src={`${process.env.PUBLIC_URL}/assets/hats/shan-hat.png`} />
+          </div>
           <img className='dev-img' src={`${process.env.PUBLIC_URL}/cowboys/shan.png`} />
           <h3 className="dev-name">Shan Hathaway</h3>
           <a className="site-link" href='https://github.com/Hathaway-Shan'>GitHub</a>
           <a className="site-link" href='https://www.linkedin.com/in/shan-hathaway/'>LinkedIn</a>
         </div>
         <div className="dev">
+          <div className="hat-container">
+            <img className="hat" src={`${process.env.PUBLIC_URL}/assets/hats/dave-hat.png`} />
+          </div>
           <img className='dev-img' src={`${process.env.PUBLIC_URL}/cowboys/dave.jpg`} />
           <h3 className="dev-name">Dave Fisher</h3>
           <a className="site-link" href='https://github.com/davejfish'>GitHub</a>
