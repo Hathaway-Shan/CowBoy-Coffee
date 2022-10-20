16faabab637e99191f7a233999be03823064e901
Hathaway-Shan <hathaway.shan@gmail.com>
2022-10-20 16:26:27 -0700

    fixed issue with menu going under guns in home

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -9,29 +9,29 @@ export default function Auth() {
   const [error, setError] = useState(null);
   const { user, setUser } = useUser();
   const { type } = useParams();
-  const typeName = type === 'sign-in' ? 'sign-in' : 'sign-up';  
+  const typeName = type === 'sign-in' ? 'sign-in' : 'sign-up';
 
   const handleSubmitAuth = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    
+
     try {
       const currentUser = await authUser(formData.get('email'), formData.get('password'), type);
       setUser(currentUser);
     } catch (e) {
       setError(e.message);
     }
-  }; 
+  };
 
   if (user) return <Redirect to={'/campfire'} />;
 
   return (
-    <div className='auth-form-container'>
-      <form className='auth-form' onSubmit={handleSubmitAuth}>
-        <label htmlFor='email'> Email:</label>
-        <input name='email'/>
-        <label htmlFor='password'> Password:</label>
-        <input type='password' name='password'/>
+    <div className="auth-form-container">
+      <form className="auth-form" onSubmit={handleSubmitAuth}>
+        <label htmlFor="email"> Email:</label>
+        <input name="email" />
+        <label htmlFor="password"> Password:</label>
+        <input type="password" name="password" />
         <button>{typeName}</button>
       </form>
       {error && <h2>{error}</h2>}
