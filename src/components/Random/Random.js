85465e221c55cfc3952fcfaf28d8d6693a7c12d0
Hathaway-Shan <hathaway.shan@gmail.com>
2022-10-20 14:25:28 -0700

    flip animation on quick draw is working

--- src/components/Random/Random.js
+++ src/components/Random/Random.js
@@ -5,29 +5,39 @@ export default function Random() {
   const { shops } = useShops();
 
   const handleRandom = async () => {
-  
-  // getRandomInt generates a random number between 0 and array.length
+    // getRandomInt generates a random number between 0 and array.length
     function getRandomInt(min, max) {
       min = Math.ceil(0);
       max = Math.floor(shops.length);
       return Math.floor(Math.random() * (max - min) + min);
     }
-  
-  // sets getRandomInt as 'i' to generate a random index in the array 
+
+    // sets getRandomInt as 'i' to generate a random index in the array
     let i = getRandomInt();
     const randomShop = shops[i];
-  
-  // immediately redirects user to the random shop detail page
+
+    // immediately redirects user to the random shop detail page
     window.location = `/campfire/${randomShop.id}`;
   };
 
   return (
-    <div className = "random-container">
+    <div className="random-container">
       <span>get a random shop</span>
       <button className="random-button" onClick={handleRandom}>
-        <p>
-          Quick Draw
-        </p>
+        <div className="quickDrawContainer">
+          <div className="quickDrawText">
+            <div className="quickDraw_letter">Q</div>
+            <div className="quickDraw_letter">u</div>
+            <div className="quickDraw_letter">i</div>
+            <div className="quickDraw_letter">c</div>
+            <div className="quickDraw_letter">k</div>
+            <div className="quickDraw_letter">d</div>
+            <div className="quickDraw_letter">r</div>
+            <div className="quickDraw_letter">a</div>
+            <div className="quickDraw_letter">w</div>
+            <div className="quickDraw_letter">!</div>
+          </div>
+        </div>
       </button>
     </div>
   );
